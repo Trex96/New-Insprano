9a2b938a88fc7baad8ec23ed764e56ab18e8b3f2
Bikash Marandi <95542691+Trex96@users.noreply.github.com>
2025-02-26 01:19:22 +0530

    First

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -6,9 +6,9 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import { slideUp } from './animation';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 export default function Home() {
-
   const firstText = useRef(null);
   const secondText = useRef(null);
   const slider = useRef(null);
@@ -44,24 +44,62 @@ export default function Home() {
   }
 
   return (
-    <motion.main variants={slideUp} initial="initial" animate="enter" className={styles.landing}>
-      <Image 
-        src="/images/background.jpg"
-        fill={true}
-        alt="background"
-      />
-      <div className={styles.sliderContainer}>
-        <div ref={slider} className={styles.slider}>
-          <p ref={firstText}>Freelance Developer -</p>
-          <p ref={secondText}>Freelance Developer -</p>
-        </div>
+    <motion.main 
+      variants={slideUp} 
+      initial="initial" 
+      animate="enter" 
+      className={styles.landing}
+    >
+      <div className={styles.imageContainer}>
+        <Image 
+          src="/images/Hero.jpg"
+          fill={true}
+          alt="background"
+          priority
+          className={styles.backgroundImage}
+        />
+        <div className={styles.gradientOverlay} />
       </div>
-      <div data-scroll data-scroll-speed={0.1} className={styles.description}>
-        <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
-        </svg>
-        <p>Freelance</p>
-        <p>Designer & Developer</p>
+
+      <div className={styles.content}>
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>
+            <Link href="/">
+              INSPRANO
+            </Link>
+          </div>
+          
+          <div className={styles.menuItems}>
+            <Link href="/about">About</Link>
+            <Link href="/services">Services</Link>
+            <Link href="/portfolio">Portfolio</Link>
+            <Link href="/contact">Contact</Link>
+          </div>
+
+          <button 
+            className={styles.contactButton}
+            onClick={() => window.location.href='/contact'}
+          >
+            Get in Touch
+          </button>
+        </nav>
+
+        <div className={styles.sliderContainer}>
+          <div ref={slider} className={styles.slider}>
+            <p ref={firstText}>Insprano X Udaan</p>
+            <p ref={secondText}>Insprano X Udaan</p>
+          </div>
+        </div>
+
+        <div 
+          data-scroll 
+          data-scroll-speed={0.1} 
+          className={styles.description}
+        >
+          <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
+          </svg>
+        </div>
       </div>
     </motion.main>
   )
